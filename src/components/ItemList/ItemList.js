8d99ec4dd215af80258f53aecf7c6cd5793f6a46
AstrayOne <fantomix293@gmail.com>
2021-03-31 17:56:56 +0300

    commit

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -3,42 +3,44 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 
-import styles from './ItemList.module.css';
 import Item from 'components/Item';
 import LoadingSpinner from 'components/LoadingSpinner';
 import LoadingError from 'components/LoadingError';
-
 import { fetchMovieList } from 'models/movieList/movieListActions';
+import styles from './ItemList.module.css';
 
 const ItemList = (props) => {
   const location = useLocation();
-  const { items } = props; 
+  const { items } = props;
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchMovieList());
-  }, [])
+  }, []);
 
-  const loading = useSelector(state => state.movieList.movieListLoading);
-  const error = useSelector(state => state.movieList.movieListError);
+  const loading = useSelector((state) => state.movieList.movieListLoading);
+  const error = useSelector((state) => state.movieList.movieListError);
 
   const parsed = queryString.parse(location.search);
 
   let filteredItems = items;
 
   if (parsed.search) {
-    filteredItems = items.filter((item) => item.title.includes(parsed.search) ||
-      item.director.includes(parsed.search) || item.releaseYear.includes(parsed.search) 
-    )
+    filteredItems = items.filter(
+      (item) =>
+        item.title.includes(parsed.search) ||
+        item.director.includes(parsed.search) ||
+        item.releaseYear.includes(parsed.search)
+    );
   }
 
-  if(error) {
-    return (<LoadingError />);
+  if (error) {
+    return <LoadingError />;
   }
 
-  if(loading) {
-    return (<LoadingSpinner />);
+  if (loading) {
+    return <LoadingSpinner />;
   }
 
   return (
